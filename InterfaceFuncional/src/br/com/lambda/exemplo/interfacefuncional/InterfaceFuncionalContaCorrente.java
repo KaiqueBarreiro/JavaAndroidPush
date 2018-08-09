93e47ca11da08539b4645776f81e6b5c8c8ed2df
E803962 <E803962@P010515.copel.nt>
2018-08-09 16:47:09 -0300

    Notificação push

--- InterfaceFuncional/src/br/com/lambda/exemplo/interfacefuncional/InterfaceFuncionalContaCorrente.java
+++
@@ -1,7 +0,0 @@
-package br.com.lambda.exemplo.interfacefuncional;
-
-@FunctionalInterface
-public interface InterfaceFuncionalContaCorrente {
-	// Por ser funcional permite somente um método abstrato
-	public CaixaAgencia atualizarSaldo(CaixaAgencia ctaA, double valor);
-}
--- InterfaceFuncional/src/br/com/lambda/exemplo/interfacefuncional/exemplointernet/OperadorDouble.java
+++
@@ -1,8 +0,0 @@
-package br.com.lambda.exemplo.interfacefuncional.exemplointernet;
-
-@FunctionalInterface
-public interface OperadorDouble {
-
-	public Double aplicar(Double a, Double b);
-
-}
